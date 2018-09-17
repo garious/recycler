239a284e5fd73b0f84d2359c704ba35e6bd115b2
Greg Fitzgerald <greg@solana.com>
2018-09-16 20:42:08 -0600

    Add AsRef and AsMut references

--- src/recycler.rs
+++ src/recycler.rs
@@ -11,6 +11,18 @@ pub struct Recyclable<'a, T: 'a + Default + Reset> {
     landfill: &'a Mutex<Vec<T>>,
 }
 
+impl<'a, T: Default + Reset> AsRef<T> for Recyclable<'a, T> {
+    fn as_ref(&self) -> &T {
+        self.val.as_ref().unwrap()
+    }
+}
+
+impl<'a, T: Default + Reset> AsMut<T> for Recyclable<'a, T> {
+    fn as_mut(&mut self) -> &mut T {
+        self.val.as_mut().unwrap()
+    }
+}
+
 impl<'a, T: Default + Reset> Drop for Recyclable<'a, T> {
     fn drop(&mut self) {
         if let Some(val) = self.val.take() {
@@ -68,7 +80,7 @@ mod tests {
     #[test]
     fn test_allocate() {
         let recycler: Recycler<Foo> = Recycler::default();
-        assert_eq!(recycler.allocate().val.as_ref().unwrap().x, 0);
+        assert_eq!(recycler.allocate().as_ref().x, 0);
     }
 
     #[test]
@@ -77,12 +89,12 @@ mod tests {
 
         {
             let mut foo = recycler.allocate();
-            foo.val.as_mut().unwrap().x = 1;
+            foo.as_mut().x = 1;
         }
         assert_eq!(recycler.landfill.lock().unwrap().len(), 1);
 
         let foo = recycler.allocate();
-        assert_eq!(foo.val.as_ref().unwrap().x, 0);
+        assert_eq!(foo.as_ref().x, 0);
         assert_eq!(recycler.landfill.lock().unwrap().len(), 0);
     }
 }

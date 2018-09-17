83ada81070c2d1510474a49471ef3fcdfe5676d0
Greg Fitzgerald <greg@solana.com>
2018-09-16 20:35:11 -0600

    Allow multiple allocations to share scope

--- src/recycler.rs
+++ src/recycler.rs
@@ -1,3 +1,5 @@
+use std::sync::Mutex;
+
 /// A function that leaves the given type in the same state as Default,
 /// but starts with an existing type instead of allocating a new one.
 pub trait Reset {
@@ -6,41 +8,44 @@ pub trait Reset {
 
 pub struct Recyclable<'a, T: 'a + Default + Reset> {
     val: Option<T>,
-    landfill: &'a mut Vec<T>,
+    landfill: &'a Mutex<Vec<T>>,
 }
 
 impl<'a, T: Default + Reset> Drop for Recyclable<'a, T> {
     fn drop(&mut self) {
         if let Some(val) = self.val.take() {
-            self.landfill.push(val);
+            self.landfill.lock().unwrap().push(val);
         }
     }
 }
 
 /// An object to minimize memory allocations.
 pub struct Recycler<T: Default + Reset> {
-    landfill: Vec<T>,
+    landfill: Mutex<Vec<T>>,
 }
 
 impl<T: Default + Reset> Default for Recycler<T> {
     fn default() -> Self {
-        Recycler { landfill: vec![] }
+        Recycler {
+            landfill: Mutex::new(vec![]),
+        }
     }
 }
 
 impl<T: Default + Reset> Recycler<T> {
-    pub fn allocate(&mut self) -> Recyclable<T> {
-        let val = Some(
-            self.landfill
-                .pop()
-                .map(|mut val| {
-                    val.reset();
-                    val
-                }).unwrap_or_default(),
-        );
+    pub fn allocate(&self) -> Recyclable<T> {
+        let val = self
+            .landfill
+            .lock()
+            .unwrap()
+            .pop()
+            .map(|mut val| {
+                val.reset();
+                val
+            }).unwrap_or_default();
         Recyclable {
-            val,
-            landfill: &mut self.landfill,
+            val: Some(val),
+            landfill: &self.landfill,
         }
     }
 }
@@ -62,24 +67,22 @@ mod tests {
 
     #[test]
     fn test_allocate() {
-        let mut recycler: Recycler<Foo> = Recycler::default();
+        let recycler: Recycler<Foo> = Recycler::default();
         assert_eq!(recycler.allocate().val.as_ref().unwrap().x, 0);
     }
 
     #[test]
     fn test_recycle() {
-        let mut recycler: Recycler<Foo> = Recycler::default();
+        let recycler: Recycler<Foo> = Recycler::default();
 
         {
             let mut foo = recycler.allocate();
             foo.val.as_mut().unwrap().x = 1;
         }
-        assert_eq!(recycler.landfill.len(), 1);
+        assert_eq!(recycler.landfill.lock().unwrap().len(), 1);
 
-        {
-            let foo = recycler.allocate();
-            assert_eq!(foo.val.as_ref().unwrap().x, 0);
-        }
-        assert_eq!(recycler.landfill.len(), 1);
+        let foo = recycler.allocate();
+        assert_eq!(foo.val.as_ref().unwrap().x, 0);
+        assert_eq!(recycler.landfill.lock().unwrap().len(), 0);
     }
 }

496267f100b6e4736bcf5c900c9898bae7229797
Greg Fitzgerald <greg@solana.com>
2018-09-16 20:18:49 -0600

    Recycle automatically via Drop trait

--- src/recycler.rs
+++ src/recycler.rs
@@ -1,33 +1,47 @@
-/// An object to minimize memory allocations.
-pub struct Recycler<T> {
-    landfill: Vec<T>,
-}
-
 /// A function that leaves the given type in the same state as Default,
 /// but starts with an existing type instead of allocating a new one.
 pub trait Reset {
     fn reset(&mut self);
 }
 
-impl<T> Default for Recycler<T> {
+pub struct Recyclable<'a, T: 'a + Default + Reset> {
+    val: Option<T>,
+    landfill: &'a mut Vec<T>,
+}
+
+impl<'a, T: Default + Reset> Drop for Recyclable<'a, T> {
+    fn drop(&mut self) {
+        if let Some(val) = self.val.take() {
+            self.landfill.push(val);
+        }
+    }
+}
+
+/// An object to minimize memory allocations.
+pub struct Recycler<T: Default + Reset> {
+    landfill: Vec<T>,
+}
+
+impl<T: Default + Reset> Default for Recycler<T> {
     fn default() -> Self {
         Recycler { landfill: vec![] }
     }
 }
 
 impl<T: Default + Reset> Recycler<T> {
-    pub fn allocate(&mut self) -> T {
-        match self.landfill.pop() {
-            Some(mut x) => {
-                x.reset();
-                x
-            }
-            None => Default::default(),
+    pub fn allocate(&mut self) -> Recyclable<T> {
+        let val = Some(
+            self.landfill
+                .pop()
+                .map(|mut val| {
+                    val.reset();
+                    val
+                }).unwrap_or_default(),
+        );
+        Recyclable {
+            val,
+            landfill: &mut self.landfill,
         }
-    }
-
-    pub fn recycle(&mut self, x: T) {
-        self.landfill.push(x);
     }
 }
 
@@ -49,7 +63,7 @@ mod tests {
     #[test]
     fn test_allocate() {
         let mut recycler: Recycler<Foo> = Recycler::default();
-        assert_eq!(recycler.allocate().x, 0);
+        assert_eq!(recycler.allocate().val.as_ref().unwrap().x, 0);
     }
 
     #[test]
@@ -58,12 +72,14 @@ mod tests {
 
         {
             let mut foo = recycler.allocate();
-            foo.x = 1;
-            recycler.recycle(foo);
+            foo.val.as_mut().unwrap().x = 1;
         }
         assert_eq!(recycler.landfill.len(), 1);
 
-        assert_eq!(recycler.allocate().x, 0);
-        assert_eq!(recycler.landfill.len(), 0);
+        {
+            let foo = recycler.allocate();
+            assert_eq!(foo.val.as_ref().unwrap().x, 0);
+        }
+        assert_eq!(recycler.landfill.len(), 1);
     }
 }
